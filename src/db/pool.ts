3c68be2a4f570e2731eac5a612fe3270e2ae06ae
ankitkumar411993-ship-it <ankitkumar411993@gmail.com>
2026-07-18 08:02:05 +0530

    Delete src directory

--- src/db/pool.ts
+++
@@ -1,8 +0,0 @@
-import { Pool } from "pg";
-import { config } from "../config";
-
-export const pool = new Pool({ connectionString: config.database.url });
-
-pool.on("error", (err) => {
-  console.error("Unexpected Postgres pool error", err);
-});
